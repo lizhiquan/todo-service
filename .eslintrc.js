86b6da258352f370d4d9a136a420d2cfe98c6823
Chi-Quyen Le <quyen.lechi@gmail.com>
2019-05-31 19:23:08 -0700

    Setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 6
+  },
+  env: {
+    commonjs: true,
+    node: true,
+    es6: true
+  },
+  extends: 'eslint:recommended',
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-console': [
+      'warn',
+      { allow: ['clear', 'info', 'error', 'dir', 'trace'] }
+    ],
+    curly: 'error',
+    'no-else-return': 'error',
+    'no-unneeded-ternary': 'error',
+    'no-useless-return': 'error',
+    'no-var': 'error',
+    'one-var': ['error', 'never'],
+    'prefer-arrow-callback': 'error',
+    'symbol-description': 'error',
+    yoda: ['error', 'never', { exceptRange: true }],
+    'prefer-const': 'error'
+  }
+};
