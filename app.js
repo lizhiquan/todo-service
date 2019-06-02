b517fcb02eb38afc771bd9ac4458b53043f7179e
Chi-Quyen Le <quyen.lechi@gmail.com>
2019-06-01 20:01:33 -0700

    Separate starting server code from app.js

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use('/user', require('./routes/user'));
 app.use('/todo', require('./routes/todo'));
 
-app.listen(port, () => {
-  console.log('Todo service is listening on port ' + port);
-});
+module.exports = app;
