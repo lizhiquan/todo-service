4f373129c640b60592b97d16705a457593fde77d
Chi-Quyen Le <quyen.lechi@gmail.com>
2019-08-08 23:17:06 -0700

    Only start the server if a db connection is established successfully

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const app = require('./app');
+const db = require('./database/index');
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log('Todo service is listening on port ' + port);
-});
+db.getConnection()
+  .then(() =>
+    app.listen(port, () => {
+      console.log('Todo service is listening on port ' + port);
+    })
+  )
+  .catch(error => console.error(error));
